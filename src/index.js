36237700ae29aff0dbbff753465f0e3992d96660
Chizh13 <antonchizhevich48@fmail.com>
2020-02-23 15:03:22 +0300

    fix: creat cod

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+
+	let str = String(n);
+    let result ='';
+
+   
+    str = String(Math.abs(n));
+    
+
+    for (let i = str.length; i > 0; i--) {
+    	result += str[i - 1];
+    }
+      
+      return Number(result);
+
 }
+
+
+
